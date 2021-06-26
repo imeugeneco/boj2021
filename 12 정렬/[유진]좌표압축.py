cd41080386874bb3f069111432630cb0093bae31
Eugene Co <imeugeneco@gmail.com>
2021-06-26 19:22:21 +0900

    풀이 수정

--- 12 정렬/[유진]좌표압축.py
+++ 12 정렬/[유진]좌표압축.py
@@ -1,6 +1,6 @@
 # 18870 좌표압축
 x = int(input())
-coords = list(int(i) for i in input().split())
+coords = list(map(int, input().split()))
 nums = sorted(set(coords))
-for i in coords:
-    print(sum(i>x for x in nums), end=' ') # 29964KB, 10624ms
+nums = {nums[i]: i for i in range(len(nums))}
+print(*[nums[i] for i in coords]) # 152716KB, 1812ms
